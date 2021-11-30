d3db1dacd4667233420ddaa832deec16ae98eaf9
carter dooley <carter.j.dooley43@gmail.com>
2021-11-30 16:54:46 -0600

    Style Updates

--- public/equations.js
+++ public/equations.js
@@ -6,8 +6,9 @@
     let c = document.getElementById('cin')
     let answer1 = document.getElementById('solin1')
     let answer2 = document.getElementById('solin2')
-    let a2 = document.getElementById('')
-   
+    let a2 = document.getElementById('a2')
+    let b2 = document.getElementById('b2')
+    let c2 = document.getElementById('c2')
    
    
        //Functions
@@ -19,9 +20,45 @@
        let resultP = (0 - bn + squreRooted) / (2 * an)
        let resultN = (0 - bn - squreRooted) / (2 * an)
        answer1.value = resultP
-        answer2.value = resultN
+       answer2.value = resultN
    }
    
+   const solvePyth = () => {
+    if(c2.value === '' && b2.value !== '' && a2.value !== ''){
+        let a = a2.value
+        let b = b2.value
+        let c = (a * a) + (b * b)
+        c = Math.sqrt(c)
+        c2.value = c
+    }else if(b2.value === '' && a2.value !== '' && c2.value !== ''){
+        let a = a2.value
+        let c = c2.value
+        let b = (c * c) - (a * a)
+        b = Math.sqrt(b)
+        b2.value = b
+    }else if(a2.value === '' && b2.value !== '' && c2.value !== ''){
+        let b = b2.value
+        let c = c2.value
+        let a = (c * c) - (b * b)
+        a = Math.sqrt(a)
+        a2.value = a
+    }else {
+        alert('Invalid Variables')
+    }
+   }
+
+   const clearQuad =() => {
+       a.value = ''
+       b.value = ''
+       c.value = ''
+       answer1.value = ''
+       answer2.value = ''
+   }
+   const clearPyth =() => {
+    a2.value = ''
+    b2.value = ''
+    c2.value = ''
+}
 
 
 
@@ -29,4 +66,6 @@
        //Event Listeners
    
    document.getElementById('solve').addEventListener('click', solveQuad)
-   
+   document.getElementById('solvep').addEventListener('click', solvePyth)
+   document.getElementById('cl1').addEventListener('click', clearQuad)
+   document.getElementById('cl2').addEventListener('click', clearPyth)
